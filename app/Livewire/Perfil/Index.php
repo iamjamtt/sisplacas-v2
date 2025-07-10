e5ae7d7742daa018cc7a293e77fefc97f29ac716
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2025-07-09 19:46:04 -0500

    Refactor el modelo de usuario en el componente de perfil para usar 'User' en lugar de 'Usuario' y actualizar la lógica de asignación de nombre y contraseña.

--- app/Livewire/Perfil/Index.php
+++ app/Livewire/Perfil/Index.php
@@ -2,7 +2,7 @@
 
 namespace App\Livewire\Perfil;
 
-use App\Models\Usuario;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Livewire\Attributes\Title;
@@ -17,7 +17,7 @@ class Index extends Component
 {
     use WithFileUploads;
 
-    public ?Usuario $usuario;
+    public ?User $usuario;
 
     #[Url(except: 'perfil', as: 'tab')]
     public string $tab = 'perfil';
@@ -41,40 +41,7 @@ class Index extends Component
     public function mount()
     {
         $this->usuario = Auth::user();
-        $this->nombre = $this->usuario->nombre_usu;
-    }
-
-    public function asignarTab(string $tab)
-    {
-        $this->tab = $tab;
-    }
-
-    public function restablecerPerfil()
-    {
-        $this->reset(['foto', 'nombre']);
-        $this->nombre = $this->usuario->nombre_usu;
-        $this->dispatch('toast', message: 'Los cambios han sido cancelados', type: 'info');
-    }
-
-    public function guardarPerfil()
-    {
-        $this->validate([
-            'nombre' => 'required|string|max:100',
-            'foto' => 'nullable|image|max:4096',
-        ]);
-
-        if ($this->foto) {
-            $folders = [
-                'avatars',
-            ];
-            $ruta_archivo = $this->usuario->avatar_usu ?? null;
-            $this->usuario->avatar_usu = subirArchivo($this->foto, $ruta_archivo, $folders);
-        }
-
-        $this->usuario->nombre_usu = $this->nombre;
-        $this->usuario->save();
-
-        $this->dispatch('toast', message: 'Perfil actualizado correctamente', type: 'success');
+        $this->nombre = $this->usuario->name;
     }
 
     public function restablecerPerfilSeguridad()
@@ -90,8 +57,8 @@ class Index extends Component
             'nueva_contrasena' => 'required|string|min:8|max:20',
         ]);
 
-        if (Hash::check($this->password, $this->usuario->contrasena_usu)) {
-            $this->usuario->contrasena_usu = Hash::make($this->nueva_contrasena);
+        if (Hash::check($this->password, $this->usuario->password)) {
+            $this->usuario->password = Hash::make($this->nueva_contrasena);
             $this->usuario->save();
         } else {
             $this->dispatch('toast', message: 'La contraseña actual no es correcta', type: 'error');
