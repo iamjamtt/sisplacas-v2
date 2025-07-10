54b43f916fa290adbe2d59520d46db67b0962f59
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2025-07-09 19:40:14 -0500

    Modificar el tamaño del encabezado en la vista de componentes a 'lg'

--- resources/views/components/head/index.blade.php
+++ resources/views/components/head/index.blade.php
@@ -7,7 +7,7 @@
 <div class="mb-4 sm:mb-4">
     <div class="mx-auto flex flex-col sm:flex-row items-stretch sm:items-center gap-3 sm:gap-2">
         <div class="flex flex-col gap-1 sm:gap-0 sm:w-1/2">
-            <flux:heading size="xl" level="1">
+            <flux:heading size="lg" level="1">
                 {{ $title }}
             </flux:heading>
 
